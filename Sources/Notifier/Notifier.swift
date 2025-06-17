ef53a83947c50406665b89b1c5e014f43ac201bd
Arthur Hakobyan <arthurhakobyan6@gmail.com>
2025-06-17 20:20:03 +0300

    Added minial start code for Notifier

--- Sources/Notifier/Notifier.swift
+++ Sources/Notifier/Notifier.swift
@@ -1,2 +1,16 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public final class Notifier {
+	static let shared = Notifier()
+	
+	private init() {}
+	
+	public func configure() {
+		// TODO: configure framework
+	}
+}
+
+extension Notifier: Sendable {}
